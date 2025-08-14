31ed81df2e82d7ce39e667c1063a668a5bd8f7f7
Angelo Luz <angelogl@gmail.com>
2025-08-14 20:43:20 -0300

    Adiciona Personagem

---
+++ Personagem.ts
@@ -0,0 +1,12 @@
+export class Personagem{
+    nome: string = "";
+    classe: string = "";
+    raca: string = "";
+    nivel: number = 0;
+    arma: string = "";
+    manaAtual: number = 0;
+    manaMaxima: number = 0;
+    vidaAtual: number = 0;
+    vidaMaxima: number = 0;
+    poderAtaque: number = 0;
+}
