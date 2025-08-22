60a6038a2eb686b93de13f3ededd3b12d45e3597
Angelo Luz <angelogl@gmail.com>
2025-08-21 21:46:07 -0300

    Adiciona menu de opções para o personagem

--- Main.ts
+++ Main.ts
@@ -1,10 +1,12 @@
 import { Personagem } from "./Personagem";
+import Prompt from "prompt-sync";
 
-const p: Personagem = new Personagem();
-p.nome = "Edécio";
+const teclado = Prompt();
+
+const p = new Personagem("Edécio");
 p.classe = "Monge";
 p.raca = "Morto-vivo";
-p.nivel = Math.floor(1+ Math.random() * 99);
+p.nivel = Math.floor(1 + Math.random() * 99);
 p.arma = "Cajado";
 p.manaMaxima = 100;
 p.manaAtual = p.manaMaxima;
@@ -12,4 +14,34 @@ p.vidaMaxima = 100;
 p.vidaAtual = p.vidaMaxima;
 p.poderAtaque = 1;
 
-console.table(p);
+while (true) {
+
+    console.log("+-----------MENU-----------+");
+    console.log("|1. Treinar Poder de Ataque|");
+    console.log("|2. Ver status             |");
+    console.log("|9. Sair                   |");
+    console.log("+--------------------------+");
+
+    const escolha: number = +teclado("Escolha uma opção do menu: ")
+
+    if (escolha === 9) {
+        break;
+    }
+    switch (escolha) {
+        case 1:
+            treinarPoderAtaque(p)
+            break;
+        case 2:
+            console.table(p);
+            break;
+
+        default:
+            console.log("Opção inválida!")
+            break;
+    }
+
+}
+
+function treinarPoderAtaque(person: Personagem): void {
+    person.treinarPoderAtaque();
+}
